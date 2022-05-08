cf77c43bca744b1f97e0ff523e0b7a81c3da8341
nickr <nicholaspriegel@gmail.com>
2022-05-08 13:46:54 -0400

    new resume

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,15 +4,24 @@ import './Nav.css'
 const Nav = () => {
   return (
     <nav>
+      
       <Link to='/'>
         <h1>Home</h1>
       </Link>
+
       <h1 id='nav-a-resume'>
-        <a href="https://drive.google.com/file/d/1YZmmZRzDi4ZX-E_zRfhhxAoX5wn7mUYj/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
+        <a
+          href="https://drive.google.com/file/d/18J3CWpdl6LyHbmcyk26Z5qjSnMt5FKkm/view?usp=sharing" target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume
+        </a>
       </h1>
+
       <Link to='/contact'>
         <h1>Contact</h1>
       </Link>
+
     </nav>
   )
 }
