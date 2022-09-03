d9a1d07c24af8543869cd713598fb0ba81648ddb
nickr <nicholaspriegel@gmail.com>
2022-09-03 18:28:03 -0400

    new resume

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,27 +3,26 @@ import './Nav.css'
 
 const Nav = () => {
   return (
-    <nav>
-      
-      <Link to='/'>
-        <h1>Home</h1>
-      </Link>
+      <nav>
+          <Link to="/">
+              <h1>Home</h1>
+          </Link>
 
-      <h1 id='nav-a-resume'>
-        <a
-          href="https://drive.google.com/file/d/1Hfz7aHUDaeAseb__QdnMTRQQXrWhySY-/view?usp=sharing" target="_blank"
-          rel="noopener noreferrer"
-        >
-          Resume
-        </a>
-      </h1>
+          <h1 id="nav-a-resume">
+              <a
+                  href="https://drive.google.com/file/d/1BFoDQX3MHkX2LsaqA7fbqStbcj4a1Qay/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                  Resume
+              </a>
+          </h1>
 
-      <Link to='/contact'>
-        <h1>Contact</h1>
-      </Link>
-
-    </nav>
-  )
+          <Link to="/contact">
+              <h1>Contact</h1>
+          </Link>
+      </nav>
+  );
 }
 
 export default Nav
