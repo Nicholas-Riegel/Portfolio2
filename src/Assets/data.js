cee46f0ea708ac4af769d5a51c955cc042ba8cf3
nickr <nicholaspriegel@gmail.com>
2023-05-12 16:04:21 -0400

    added new project

--- src/Assets/data.js
+++ src/Assets/data.js
@@ -36,23 +36,14 @@ const projectsArray = [
         preview:'./pictures/quote-generator.jpg'
     },
     {
-        name: "Memory Game",
+        name: "Learning Tool",
         tech: "React",
-        gitHubUrl: "https://github.com/Nicholas-Riegel/memory_game_react",
-        viewAt: "https://nicholas-riegel.github.io/memory_game_react/",
-        description: "This little memory game uses the Egyptian gods to test your memory! Try to click on each god only once! After each click the cards are shuffled. There is a score-keeper, a high-score, and a reset button.",
+        gitHubUrl: "https://github.com/Nicholas-Riegel/egyptian-deities",
+        viewAt: "https://nicholas-riegel.github.io/egyptian-deities/",
+        description: "This tool could be used to learn many things. I have always wanted to learn more about Egyptian mythology. So this is a fun little project to help people learn some of the most important Ancient Egyptian deities!",
         categories: ["React"],
-        preview: './pictures/memory-game.jpg',
+        preview: './pictures/egypt.jpg',
     },
-    // {
-    //     name: "Inventory Tracker",
-    //     tech: "Node.js, Express, Mongoose, MongoDB, Pug",
-    //     gitHubUrl: "https://github.com/Nicholas-Riegel/inventory_app_node_express_pug",
-    //     viewAt: "https://inventory-app0.herokuapp.com/",
-    //     description: "This app is designed to keep track of products in inventory. User may choose names of products and categories the products are listed under. Any product or category can be created, read, updated and deleted (i.e. complete CRUD operations). App includes certain protections. Products in a category must be deleted before category can be deleted, and quantity of product must be zero before product can be deleted. As it is hosted on Heroku, app may take a few seconds to upload.",
-    //     categories: ["Node.js", "Express", "Mongoose", "MongoDB", "Pug"],
-    //     preview:'./pictures/inventory-tracker.jpg'
-    // },
     {
         name: "Weather App",
         tech: "JavaScript, HTML, CSS",
@@ -62,15 +53,6 @@ const projectsArray = [
         categories: ["JavaScript, HTML, CSS"],
         preview: './pictures/weather-app.jpg',
     },
-    // {
-    //     name: "Project Manager",
-    //     tech: "Vanilla Javascript, HTML, CSS",
-    //     gitHubUrl: "https://github.com/Nicholas-Riegel/project-manager",
-    //     viewAt: "https://nicholas-riegel.github.io/project-manager/",
-    //     description: "More complex (and useful!) than a simple To-Do App, this project manager allows users to group tasks into different projects. Each project as a name, description, due date and priority, along with the todos. App uses Local Storage to save user's inputs.",
-    //     categories: ["JavaScript"],
-    //     preview:'./pictures/project-manager.jpg'
-    // },
 ]
 
 export default projectsArray
